169a18ee727e87d938de13bf27e7c243ed66839f
kika <kika@kikadeMacBook-Pro.local>
2025-07-22 11:52:57 +0800

    Revert "test: import only part of nodes"
    This reverts commit 9959b0d26fcd50839331734b3907c0c25ab9445b.

--- node_mappings.py
+++ node_mappings.py
@@ -3,10 +3,10 @@ try:
     from .nodes.nodes_aspect_ratio import *
     from .nodes.nodes_list import *
     from .nodes.nodes_lora import *
-    # from .nodes.nodes_controlnet import *
+    from .nodes.nodes_controlnet import *
     from .nodes.nodes_pipe import *
     from .nodes.nodes_sdxl import *
-    # from .nodes.nodes_model_merge import *
+    from .nodes.nodes_model_merge import *
     from .nodes.nodes_upscale import *
     from .nodes.nodes_xygrid import *
     from .nodes.nodes_legacy import *
@@ -23,27 +23,27 @@ try:
 except ImportError:
     print("\033[34mComfyroll Studio: \033[92mFailed to load Graphics nodes\033[0m")
 
-# try:
-#     from .nodes.nodes_animation_interpolation import *
-#     from .nodes.nodes_animation_io import *
-#     from .nodes.nodes_animation_prompt import *
-#     from .nodes.nodes_animation_schedulers import *
-#     from .nodes.nodes_animation_schedules import *
-#     from .nodes.nodes_animation_lists import *
-#     from .nodes.nodes_animation_utils import *
-#     from .nodes.nodes_animation_cyclers import *
-# except ImportError:
-#     print("\033[34mComfyroll Studio: \033[92mFailed to load Animation nodes\033[0m")
+try:
+    from .nodes.nodes_animation_interpolation import *
+    from .nodes.nodes_animation_io import *
+    from .nodes.nodes_animation_prompt import *
+    from .nodes.nodes_animation_schedulers import *
+    from .nodes.nodes_animation_schedules import *
+    from .nodes.nodes_animation_lists import *
+    from .nodes.nodes_animation_utils import *
+    from .nodes.nodes_animation_cyclers import *
+except ImportError:
+    print("\033[34mComfyroll Studio: \033[92mFailed to load Animation nodes\033[0m")
     
-# try:
-#     from .nodes.nodes_utils_logic import *
-#     from .nodes.nodes_utils_index import *
-#     from .nodes.nodes_utils_conversion import *
-#     from .nodes.nodes_utils_random import *
-#     from .nodes.nodes_utils_text import *
-#     from .nodes.nodes_utils_other import *
-# except ImportError:
-#     print("\033[34mComfyroll Studio: \033[92mFailed to load Utility nodes\033[0m")
+try:
+    from .nodes.nodes_utils_logic import *
+    from .nodes.nodes_utils_index import *
+    from .nodes.nodes_utils_conversion import *
+    from .nodes.nodes_utils_random import *
+    from .nodes.nodes_utils_text import *
+    from .nodes.nodes_utils_other import *
+except ImportError:
+    print("\033[34mComfyroll Studio: \033[92mFailed to load Utility nodes\033[0m")
 
 NODE_CLASS_MAPPINGS = { 
     ### Core Nodes
